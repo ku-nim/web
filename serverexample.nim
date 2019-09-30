29d4845d01a07b5e0515a7b74aacc8bd6803eeb1
murata <murata@kmc.gr.jp>
2019-09-30 21:36:36 +0900

    favicon

--- serverexample.nim
+++ serverexample.nim
@@ -1,13 +1,20 @@
 import asynchttpserver, asyncdispatch
 import json
+import os
 
 var server = newAsyncHttpServer()
 
 proc handler(req: Request) {.async.} =
-  if req.url.path == "/hello-world":
+  case req.url.path:
+  of "/hello-world":
     let msg = %* {"message": "Hello World"}
     let headers = newHttpHeaders([("Content-Type","application/json")])
     await req.respond(Http200, $msg, headers)
+  of "/favicon.ico":
+    let f = open("./favicon.ico",FileMode.fmRead)
+    defer: f.close()
+    let headers = newHttpHeaders([("Content-Type","image/png")])
+    await req.respond(Http200,f.readAll(),headers)
   else:
     await req.respond(Http404, "Not Found")
 
