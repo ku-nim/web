fbf77d9f3d9be4c29ad8499ff065d3792cc8468e
nonylene <nonylene@gmail.com>
2019-09-30 20:56:01 +0900

    Use PORT environment variable

--- serverexample.nim
+++ serverexample.nim
@@ -1,14 +1,16 @@
 import asynchttpserver, asyncdispatch
 import json
+import os
+import strutils
 
 var server = newAsyncHttpServer()
 
 proc handler(req: Request) {.async.} =
-  if req.url.path == "/hello-world":
+  if req.url.path == "/":
     let msg = %* {"message": "Hello World"}
-    let headers = newHttpHeaders([("Content-Type","application/json")])
+    let headers = newHttpHeaders([("Content-Type", "application/json")])
     await req.respond(Http200, $msg, headers)
   else:
-    await req.respond(Http404, "Not Found")
+    await req.respond(Http404, "Not Found !!!!")
 
-waitFor server.serve(Port(8080), handler)
+waitFor server.serve(Port(parseInt(os.getEnv("PORT", "8080"))), handler)
