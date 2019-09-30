9522aff28eeb9a5d372471c4eba24276c29ca0c6
murata <murata@kmc.gr.jp>
2019-09-30 22:45:41 +0900

    ページ完成！

--- server.nim
+++ server.nim
@@ -2,16 +2,36 @@ import asynchttpserver, asyncdispatch
 import json
 import os
 import strutils
-
+import pages
 var server = newAsyncHttpServer()
 
 proc handler(req: Request) {.async.} =
-  if req.url.path == "/":
+  if req.url.path.endsWith(".jpg"):
+    # NOTE: これがワイのサニタイズや！！
+    let f = open("./" & req.url.path.replace("/",""),FileMode.fmRead)
+    defer: f.close()
+    let headers = newHttpHeaders([("Content-Type","image/jpg")])
+    await req.respond(Http200,f.readAll(),headers)
+    return
+  case req.url.path:
+  of "/":
+    let headers = newHttpHeaders([("Content-Type","text/html")])
+    await req.respond(Http200, pages.index(), headers)
+    return
+  of "/hello-world":
     let msg = %* {"message": "Hello World"}
-    let headers = newHttpHeaders([("Content-Type", "application/json")])
+    let headers = newHttpHeaders([("Content-Type","application/json")])
     await req.respond(Http200, $msg, headers)
+    return
+  of "/favicon.ico":
+    let f = open("./favicon.ico",FileMode.fmRead)
+    defer: f.close()
+    let headers = newHttpHeaders([("Content-Type","image/png")])
+    await req.respond(Http200,f.readAll(),headers)
+    return
   else:
-    await req.respond(Http404, "Not Found !!!!")
+    await req.respond(Http404, "Not Found")
 
-echo parseInt(os.getEnv("PORT"))
-waitFor server.serve(Port(parseInt(os.getEnv("PORT", "8080"))), handler)
+let port = parseInt(os.getEnv("PORT", "8080"))
+echo port
+waitFor server.serve(Port(port), handler)
