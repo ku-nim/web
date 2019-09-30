fdc7f1af034ac5694ce34672210f11e1537db7cb
nonylene <nonylene@gmail.com>
2019-09-30 22:31:09 +0900

    Enforce https

--- server.nim
+++ server.nim
@@ -2,10 +2,20 @@ import asynchttpserver, asyncdispatch
 import json
 import os
 import strutils
+import uri
 
 var server = newAsyncHttpServer()
 
 proc handler(req: Request) {.async.} =
+  if "http" in req.headers.getOrDefault("X-Forwarded-Proto"):
+    let httpsUri = parseUri("https://" & req.headers["host"]) / req.url.path
+    let headers = newHttpHeaders([
+      ("Location", $httpsUri),
+      ("Strict-Transport-Security", "max-age=31536000"),
+      ])
+    await req.respond(Http301, "", headers)
+    return
+
   if req.url.path == "/":
     let msg = %* {"message": "Hello World"}
     let headers = newHttpHeaders([("Content-Type", "application/json")])
@@ -13,5 +23,4 @@ proc handler(req: Request) {.async.} =
   else:
     await req.respond(Http404, "Not Found !!!!")
 
-echo parseInt(os.getEnv("PORT"))
 waitFor server.serve(Port(parseInt(os.getEnv("PORT", "8080"))), handler)
